068bf1d719760557a4d738cd24fc4264b42d3a90
Arthur <arthurchristiand@gmail.com>
2026-09-18 16:30:48 +0800

    fixed remarks card bug

--- pages/scan.php
+++ pages/scan.php
@@ -73,7 +73,7 @@ if ($taskId <= 0) {
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&display=swap" rel="stylesheet">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
         <link rel="stylesheet" href="../styles/app.css?v=15">
-        <link rel="stylesheet" href="../styles/scan.css?v=6">
+        <link rel="stylesheet" href="../styles/scan.css?v=7">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"></script>
         <script src="../scripts/theme.js?v=6"></script>
     </head>
@@ -238,7 +238,7 @@ $pendingOrActiveRows = array_values(array_filter($assignedRows, fn($r) => $r['st
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../styles/app.css?v=15">
-    <link rel="stylesheet" href="../styles/scan.css?v=6">
+    <link rel="stylesheet" href="../styles/scan.css?v=7">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"></script>
     <script src="../scripts/theme.js?v=6"></script>
 </head>
