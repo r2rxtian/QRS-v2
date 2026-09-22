c5a70ab681115dd4cfeaba6186f8c8aee00e7349
Arthur <arthurchristiand@gmail.com>
2026-09-22 15:26:20 +0800

    dark mode toggle improved design

--- pages/manage_locations.php
+++ pages/manage_locations.php
@@ -61,7 +61,7 @@ $statTotal = count($locations);
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../styles/app.css?v=15">
+    <link rel="stylesheet" href="../styles/app.css?v=28">
     <link rel="stylesheet" href="../styles/manage_locations.css?v=2">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"></script>
     <script src="../scripts/theme.js?v=6"></script>
